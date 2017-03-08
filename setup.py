e3776878fde94f633cd00895544051a2eddf99b0
KeironO <keiron.oshea@gmail.com>
2017-03-08 16:58:51 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=utf-8
+import sys
+from copy import copy
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "dimspy",
+    version="0.1",
+    author="Keiron O'Shea",
+    author_email="keo7@aber.ac.uk",
+    url="https://www.github.com/KeironO/DIMSpy",
+    download_url="https://www.github.com/KeironO/DIMSpy/zipball/master",
+    description="",
+    long_description="",
+    packages=["dimspy"],
+    include_package_data=True
+)
