c4610bb6f37be87f554640020fb9d13b7de040b5
Keiron O'Shea <keiron.oshea@gmail.com>
2018-05-15 13:54:55 +0100

    Updated setup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
 setup(
-    name='dimspy',
-    version='0.0.1',
-    packages=['dimspy'],
-    url='http://www.github.com/KeironO/dimspy',
-    license='MIT',
+    name='dimepy',
+    version='0.0.5',
+    packages=['dimepy'],
+    url='http://www.github.com/KeironO/dimepy',
+    license='GPLv2',
     author='Keiron O\'Shea',
     author_email = 'keo7@aber.ac.uk',
     description = 'Python package for the high-thoroughput nontargeted metabolite fingerprinting of nominal mass direct injection mass spectrometry.'
