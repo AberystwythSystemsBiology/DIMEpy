8323b08498e175a9fbd27807cd5e54315206ce00
Keiron O'Shea <keiron.oshea@gmail.com>
2018-06-01 19:06:29 +0100

    Updated setup

--- setup.py
+++ setup.py
@@ -7,33 +7,31 @@ try:
     import pypandoc
     long_description = pypandoc.convert("README.md", "rst")
 except (IOError, ImportError):
-    long_description=open("README.md").read()
-
-
+    long_description = open("README.md").read()
 
 setup(
-    name='dimepy',
-    version='0.0.9.5b',
+    name="dimepy",
+    version="0.1",
     url='http://www.github.com/KeironO/dimepy',
-    license='GPLv2',
+    license="GPLv2",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=open("requirements.txt").read().splitlines(),
-    platforms=['Windows', 'UNIX'],
+    platforms=["Windows", "UNIX"],
     long_description=long_description,
-    author='Keiron O\'Shea',
-    author_email = 'keo7@aber.ac.uk',
-    description = 'Python package for the high-thoroughput nontargeted metabolite fingerprinting of nominal mass direct injection mass spectrometry.',
+    author="Keiron O'Shea",
+    author_email="keo7@aber.ac.uk",
+    description="Python package for the high-thoroughput nontargeted metabolite fingerprinting of nominal mass direct injection mass spectrometry.",
     classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Development Status :: 2 - Pre-Alpha',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-        'Operating System :: OS Independent',
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Developers'
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)"",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers"
     ]
 )
