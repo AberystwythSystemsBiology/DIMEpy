e38397459d795103ade78316b742875a0e32fb83
KeironO <keiron.oshea@gmail.com>
2019-07-31 16:44:52 +0100

    Begun setting up tests

---
+++ tests/spectrum_test.py
@@ -0,0 +1,8 @@
+import unittest
+
+class SpectrumTest(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/spectrumlist_test.py
@@ -0,0 +1,9 @@
+import unittest
+
+class SpectrumListTest(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
