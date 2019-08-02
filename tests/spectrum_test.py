901b73cbcd19e2dd95119034f3ea2efb6ff20241
KeironO <keiron.oshea@gmail.com>
2019-08-02 12:56:59 +0100

    Rel path for testing

--- tests/spectrum_test.py
+++ tests/spectrum_test.py
@@ -1,26 +1,31 @@
 import unittest
 from dimepy import Spectrum
+import os
+
+scrpt_dir = os.path.dirname(os.path.abspath(__file__))
+
+mzml_fp = os.path.join(scrpt_dir, "data/example.mzML")
 
 class SpectrumTest(unittest.TestCase):
 
     def test_loading(self):
-        spectrum = Spectrum("data/example.mzML", "test")
+        spectrum = Spectrum(mzml_fp, "test")
         spectrum.load_scans()
 
     def test_negative_polarity(self):
         # The entire spectrum is positive polarity, so this should return False
-        spectrum = Spectrum("data/example.mzML", "test")
+        spectrum = Spectrum(mzml_fp, "test")
         spectrum.limit_polarity("negative")
         self.assertFalse(True in spectrum.to_use)
 
     def test_positive_polarity(self):
         # The entire spectrum is positive, so this should return False
-        spectrum = Spectrum("data/example.mzML", "test")
+        spectrum = Spectrum(mzml_fp, "test")
         spectrum.limit_polarity("positive")
         self.assertFalse(False in spectrum.to_use)
 
     def test_binning(self):
-        spectrum = Spectrum("data/example.mzML", "test")
+        spectrum = Spectrum(mzml_fp, "test")
         spectrum.limit_polarity("negative")
         spectrum._to_use[0] = True
         spectrum.load_scans()
